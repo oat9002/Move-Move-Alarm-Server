82429eec5cc77589964d61bd05a251dc39b6f9b0
oat9002 <oat9002@hotmail.com>
2016-05-15 00:33:53 +0700

    initial git

---
+++ src/main/java/movealarm/kmitl/net/MoveAlarmServerApplication.java
@@ -0,0 +1,14 @@
+package movealarm.kmitl.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveAlarmServerApplication {
+
+    public static void main(String[] args) {
+        SQLInquirer sqlInquirer = SQLInquirer.getInstance();
+        System.out.println("database server connection : " + ((sqlInquirer.isConnecting()) ? "connected" : "not connected"));
+        SpringApplication.run(MoveAlarmServerApplication.class, args);
+    }
+}
