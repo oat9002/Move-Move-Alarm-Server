1c984beb186d0d6314c85c2b3ce91ebc416fd4df
oat9002 <oat9002@hotmail.com>
2016-05-30 15:30:48 +0700

    [#10]Finished but not test

--- src/main/java/movealarm/kmitl/net/GroupActivityProgress.java
+++ src/main/java/movealarm/kmitl/net/GroupActivityProgress.java
@@ -2,24 +2,32 @@ package movealarm.kmitl.net;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
 public class GroupActivityProgress extends Model {
+    private double totalExerciseTime = 0;
     private int numberOfAccept = 0;
-    private int numberOfCancel = 0;
-    private int cancelActivity = 0;
+    private int totalActivity = 0;
+    private int neck = 0;
+    private int shoulder = 0;
+    private int chest_back = 0;
+    private int wrist = 0;
+    private int waist = 0;
+    private int hip_leg_calf = 0;
     private Group group;
-    private Date date;
 
     public GroupActivityProgress()
     {
-        this.tableName = "groupActivity_progress";
+        this.tableName = "groupActivity_progress_week";
 
-        this.addRequiredField("numberOfAccept");
-        this.addRequiredField("cancelActivity");
-        this.addRequiredField("groupID");
+        //this.addRequiredField("numberOfAccept");
+        //this.addRequiredField("cancelActivity");
+        //this.addRequiredField("groupID");
     }
+
+
 
     public static GroupActivityProgress findByGroup(Group group)
     {
@@ -32,14 +40,18 @@ public class GroupActivityProgress extends Model {
         HashMap<String, Object> progressData = data.get(0);
 
         GroupActivityProgress model = new GroupActivityProgress();
-        model.id = converter.toInt(progressData.get("id"));
-        model.group = group;
-        model.numberOfAccept = converter.toInt(progressData.get("numberOfAccept"));
-        model.numberOfCancel = converter.toInt(progressData.get("numberOfCancel"));
-        model.cancelActivity = converter.toInt(progressData.get("cancelActivity"));
-        model.date = (Date) progressData.get("date");
-        model.createdDate = (Date) progressData.get("createdDate");
-        model.modifiedDate = (Date) progressData.get("modifiedDate");
+        model.setID(converter.toInt(progressData.get("id")));
+        model.setTotalExerciseTime(converter.toDouble(progressData.get("totalExerciseTime")));
+        model.setTotalActivity(converter.toInt(progressData.get("totalActivity")));
+        model.setNumberOfAccept(converter.toInt(progressData.get("numberOfAccept")));
+        model.setNeck(converter.toInt(progressData.get("neck")));
+        model.setShoulder(converter.toInt(progressData.get("shoulder")));
+        model.setChest_back(converter.toInt(progressData.get("chest_back")));
+        model.setWrist(converter.toInt(progressData.get("wrist")));
+        model.setWaist(converter.toInt(progressData.get("waist")));
+        model.setHip_leg_calf(converter.toInt(progressData.get("hip_leg_calf")));
+        model.setCreatedDate((Date)progressData.get("createdDate"));
+        model.setModidiedDate((Date)progressData.get("modifiedDate"));
 
         return model;
     }
@@ -52,12 +64,18 @@ public class GroupActivityProgress extends Model {
         SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         HashMap<String, Object> temp = new HashMap<>();
 
+        temp.put("totalExerciseTime", totalExerciseTime);
+        temp.put("totalActivity", totalActivity);
         temp.put("numberOfAccept", numberOfAccept);
-        temp.put("numberOfCancel", numberOfCancel);
-        temp.put("cancelActivity", cancelActivity);
+        temp.put("neck", neck);
+        temp.put("shoulder", shoulder);
+        temp.put("chest_back", chest_back);
+        temp.put("wrist", wrist);
+        temp.put("waist", waist);
+        temp.put("hip_leg_calf", hip_leg_calf);
         temp.put("groupID", group.getID());
-        if(date != null)
-            temp.put("date", sdf.format(date));
+        if(modifiedDate != null)
+        temp.put("modifiedDate", sdf.format(modifiedDate));
         if(modifiedDate != null)
             temp.put("modifiedDate", sdf2.format(modifiedDate));
 
@@ -70,31 +88,71 @@ public class GroupActivityProgress extends Model {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         HashMap<String, Object> temp = new HashMap<>();
 
+        temp.put("totalExerciseTime", totalExerciseTime);
+        temp.put("totalActivity", totalActivity);
         temp.put("numberOfAccept", numberOfAccept);
-        temp.put("numberOfCancel", numberOfCancel);
-        temp.put("cancelActivity", cancelActivity);
-        temp.put("group", group.getGeneralValues());
-        if(date != null)
-            temp.put("date", sdf.format(date));
+        temp.put("neck", neck);
+        temp.put("shoulder", shoulder);
+        temp.put("chest_back", chest_back);
+        temp.put("wrist", wrist);
+        temp.put("waist", waist);
+        temp.put("hip_leg_calf", hip_leg_calf);
 
         return temp;
+    }
+
+    @Override
+    public HashMap<String, Object> save() //save all progresses
+    {
+        HashMap<String, Object> requiredFields = checkRequiredFields(); //check if some required fields still not filled
+        if (requiredFields != null)
+            return requiredFields; //return name of need required fields
+        GroupActivityProgress progress_check = GroupActivityProgress.findByGroup(getGroup());
+        if (progress_check == null) { //if this model has never been saved before
+            HashMap<String, Object> temp = modelCollection.create(this); //create row on the database first and receive id, created date
+            if (temp == null) //if an error has occurred while inserting data to the database
+                return StatusDescription.createProcessStatus(false, "Cannot save due to a database error.");
+            id = Integer.parseInt("" + temp.get("id")); //set id and created date of this model
+            return StatusDescription.createProcessStatus(true); //return new model
+        }
+
+        Calendar createdDate_temp = Calendar.getInstance();
+        createdDate_temp.setTime(progress_check.getCreatedDate());
+        Calendar today = Calendar.getInstance();
+        if(today.YEAR == createdDate_temp.YEAR) {
+            if (today.WEEK_OF_YEAR == createdDate_temp.WEEK_OF_YEAR) { //save to db in the same week.
+                //set all stuff
+                GroupActivityProgress progress = GroupActivityProgress.findByGroup(getGroup());
+                setID(progress.getID());
+                setTotalExerciseTime(getTotalExerciseTime() + progress.getTotalExerciseTime());
+                setTotalActivity(getTotalActivity() + progress.getTotalActivity());
+                setNumberOfAccept(getNumberOfAccept() + progress.getNumberOfAccept());
+                setNeck(getNeck() + progress.getNeck());
+                setShoulder(getShoulder() + progress.getShoulder());
+                setChest_back(getChest_back() + progress.getChest_back());
+                setWrist(getWrist() + progress.getWrist());
+                setWaist(getWaist() + progress.getWaist());
+                setHip_leg_calf(getHip_leg_calf() + progress.getHip_leg_calf());
+                HashMap<String, Object> status = StatusDescription.createProcessStatus(modelCollection.save(this));
+                return status;
+            }
+        }
+        //save to db different week or year(Have to delete old row and create a new one.)
+        GroupActivityProgress progress = GroupActivityProgress.findByGroup(getGroup());
+        HashMap<String, Object> status = StatusDescription.createProcessStatus(modelCollection.delete(progress));
+        HashMap<String, Object> temp = modelCollection.create(this); //create row on the database first and receive id, created date
+        if (temp == null) //if an error has occurred while inserting data to the database
+            return StatusDescription.createProcessStatus(false, "Cannot save due to a database error.");
+
+        id = Integer.parseInt("" + temp.get("id")); //set id and created date of this model
+        createdDate = (Date) temp.get("created_date");
+        status.put("status_create", true);
+        return status; //return new model
     }
 
     public void setNumberOfAccept(int numberOfAccept)
     {
         this.numberOfAccept = numberOfAccept;
-        updateModifiedDate();
-    }
-
-    public void setNumberOfCancel(int numberOfCancel)
-    {
-        this.numberOfCancel = numberOfCancel;
-        updateModifiedDate();
-    }
-
-    public void setCancelActivity(int cancelActivity)
-    {
-        this.cancelActivity = cancelActivity;
         updateModifiedDate();
     }
 
@@ -104,9 +162,106 @@ public class GroupActivityProgress extends Model {
         updateModifiedDate();
     }
 
-    public void setDate(Date date)
-    {
-        this.date = date;
+    public double getTotalExerciseTime() {
+        return totalExerciseTime;
+    }
+
+    public void setTotalExerciseTime(double totalExerciseTime) {
+        this.totalExerciseTime = totalExerciseTime;
+        updateModifiedDate();
+    }
+
+    public int getNumberOfAccept() {
+        return numberOfAccept;
+    }
+
+    public int getTotalActivity() {
+        return totalActivity;
+    }
+
+    public void setTotalActivity(int totalActivity) {
+        this.totalActivity = totalActivity;
+        updateModifiedDate();
+    }
+
+    public int getNeck() {
+        return neck;
+    }
+
+    public void setNeck(int neck) {
+        this.neck = neck;
+        updateModifiedDate();
+    }
+
+    public int getShoulder() {
+        return shoulder;
+    }
+
+    public void setShoulder(int shoulder) {
+        this.shoulder = shoulder;
+        updateModifiedDate();
+    }
+
+    public int getChest_back() {
+        return chest_back;
+    }
+
+    public void setChest_back(int chest_back) {
+        this.chest_back = chest_back;
+        updateModifiedDate();
+    }
+
+    public int getWrist() {
+        return wrist;
+    }
+
+    public void setWrist(int wrist) {
+        this.wrist = wrist;
+        updateModifiedDate();
+    }
+
+    public int getWaist() {
+        return waist;
+    }
+
+    public void setWaist(int waist) {
+        this.waist = waist;
+        updateModifiedDate();
+    }
+
+    public int getHip_leg_calf() {
+        return hip_leg_calf;
+    }
+
+    public void setHip_leg_calf(int hip_leg_calf) {
+        this.hip_leg_calf = hip_leg_calf;
+        updateModifiedDate();
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date date) {
+        this.createdDate = date;
+        updateModifiedDate();
+    }
+
+    public Date getModifiedDate() {
+        return this.modifiedDate;
+    }
+
+    public void setModidiedDate(Date date) {
+        this.modifiedDate = date;
         updateModifiedDate();
     }
 
@@ -115,8 +270,8 @@ public class GroupActivityProgress extends Model {
         this.numberOfAccept += time;
     }
 
-    public void increaseCancelTime(int time)
+    /*public void increaseCancelTime(int time)
     {
         this.cancelActivity += time;
-    }
+    }*/
 }
