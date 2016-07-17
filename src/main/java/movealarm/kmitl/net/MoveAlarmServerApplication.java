6fd24519c8cb92738b2c5c3bae9ac2f74211f47e
Oattt <oat9002@gmail.com>
2016-07-17 13:10:30 +0700

    Reset git cache

--- src/main/java/movealarm/kmitl/net/MoveAlarmServerApplication.java
+++ src/main/java/movealarm/kmitl/net/MoveAlarmServerApplication.java
@@ -11,12 +11,5 @@ public class MoveAlarmServerApplication {
         SQLInquirer sqlInquirer = SQLInquirer.getInstance();
         System.out.println("database server connection : " + ((sqlInquirer.isConnecting()) ? "connected" : "not connected"));
         SpringApplication.run(MoveAlarmServerApplication.class, args);
-        /*HashMap<String, Object> user = new HashMap<>();
-        HashMap<String, Object> JSON = new HashMap<>();
-        user.put("id", 1);
-        JSON.put("user", Converter.getInstance().HashMapToJSON(user));
-        JSON.put("type", 0);
-        UserProgressController controller = new UserProgressController();
-        controller.getByUser(Converter.getInstance().HashMapToJSON(JSON));*/
     }
 }
